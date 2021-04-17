9de8335e031399c109d81cd31fb8bfe6cf1a011e
Stas Vasilev <vasilyevsd@gmail.com>
2021-04-18 01:23:34 +0300

    opening cells

--- src/components/GameField/GameField.tsx
+++ src/components/GameField/GameField.tsx
@@ -1,16 +1,48 @@
+import { observer } from 'mobx-react-lite';
 import React from 'react';
+import { FixedSizeGrid as Grid, GridChildComponentProps } from 'react-window';
+import { field } from '../../store/GameField';
+import { cn as createCn } from '@bem-react/classname'
 
 // import {field} from '../../store/GameField';
 
 // export {field};
 
-export const GameField: React.FC = () => {
+// export const GameField: React.FC = () => {
+//     return (
+//         <div className="Field">
+//             {Array(10000).fill(undefined).map((v, i) => (
+//                 <div key={i} />
+//             ))}
+//         </div>
+//     );
+// }
+
+const cn = createCn('cell');
+
+const Cell: React.FC<GridChildComponentProps> = observer(({ columnIndex, rowIndex, style }) => {
+    const cell = field.getCell(columnIndex, rowIndex);
     return (
-        <div className="Field">
-            {Array(10000).fill(undefined).map((v, i) => (
-                <div key={i} />
-            ))}
+        <div
+            className={cn()}
+            style={style}
+            onClick={() => cell.open()}
+        >
+            {cell.isOpen ? 'o' : 'c'}
         </div>
     );
-}
+});
 
+export const GameField: React.FC = () => (
+    <Grid
+      className="Grid"
+      columnCount={10000}
+      columnWidth={16}
+      height={600}
+      rowCount={10000}
+      rowHeight={16}
+      width={800}
+    >
+      {Cell}
+    </Grid>
+  );
--- src/store/GameField.ts
+++ src/store/GameField.ts
@@ -1,9 +1,30 @@
-import { makeObservable, observable } from "mobx";
+import { action, makeObservable, observable } from "mobx";
 
 export class GameField {
+    
+    // constructor() {
+    //     makeObservable(this, {
+    //         openCell: action
+    //     })
+    // }
     cells: Map<number, Cell> = new Map();
 
-    
+    // openCell = (x: number, y: number) => {
+    //     const cell = this.getCell(x, y);
+    //     cell.isOpen = true;
+    // }
+
+    sizeX = 10000;
+    sizeY = 10000;
+    getCell(x: number, y: number) {
+        const index = x * this.sizeX + y;
+        if (!this.cells.has(index)) {
+            const cell = new Cell()
+            this.cells.set(index, cell);
+            return cell;
+        }
+        return this.cells.get(index)!;
+    }
     // cells: {isOpen: boolean}[][]
 
     // constructor() {
@@ -17,9 +38,15 @@ export const field = new GameField();
 
 class Cell {
     constructor() {
+        // console.log(numOfCells);
         makeObservable(this, {
-            isOpen: observable
+            isOpen: observable,
+            open: action
         })
+    }
+
+    open() {
+        this.isOpen = true;
     }
 
     isOpen = false;
